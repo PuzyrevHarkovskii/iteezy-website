c50a90b626a91d5de9f81e01ffbe7c3a4c7d3439
Владислав <pahnetstarushkami@bk.ru>
2024-04-18 00:59:57 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { FloatingNav } from "@/components/ui/floating-nav";
+import Image from "next/image";
+import { FloatingNavDemo } from "./nav";
+
+export default function Home() {
+  return (
+    <div className="">
+    <FloatingNavDemo/>
+    </div>
+  );
+}
