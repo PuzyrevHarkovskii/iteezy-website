83c598949475fcb8af324a2ee4a1a4df2a87592d
Владислав <pahnetstarushkami@bk.ru>
2024-04-18 14:36:32 +0300

    new features

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,37 @@
 
+import Questions from "@/components/chakra-ui/FAQ";
+import Footer from "@/components/chakra-ui/footer";
 import { HeroParallaxDemo } from "@/components/hero";
+import { SparklesPreview } from "@/components/name";
 import { NavbarDemo } from "@/components/nav";
 import { StickyScrollRevealDemo } from "@/components/ourProgramms";
 import { InfiniteMovingCardsDemo } from "@/components/reviews";
-import { HeroParallax } from "@/components/ui/hero-parallax";
 import { TextParallaxContentExample } from "@/components/ui/Features";
-import Image from "next/image";
+import { SparklesCore } from "@/components/ui/sparkles";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function Home() {
+
+  
   return (
+
+    
     <>
+    
     <NavbarDemo/>
-    <HeroParallaxDemo/>
+    
     <TextParallaxContentExample/>
+    <InfiniteMovingCardsDemo/>
+    <StickyScrollRevealDemo/>
+    <ChakraProvider>
+    <Questions/>
+    <div> 
+    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A1310ee24fa0e4717f56a401a049c366818ae5442e279ee96ee2a602ef6a80227&amp;source=constructor" width="100%" height="400" ></iframe>
+    </div>
+    
+      
+      <Footer/>
+    </ChakraProvider>
     </>
   );
 }
