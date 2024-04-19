f5490ff9e332bfd32cc2f2924118ff50b11c1e61
Владислав <pahnetstarushkami@bk.ru>
2024-04-19 12:50:59 +0300

    new sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 
+import Contact from "@/components/chakra-ui/ContactUs";
 import Questions from "@/components/chakra-ui/FAQ";
+import GridBlurredBackdrop from "@/components/chakra-ui/Testimonials";
 import Footer from "@/components/chakra-ui/footer";
+import ShuffleHero from "@/components/herosection";
 import { StickyScrollRevealDemo } from "@/components/ourProgramms";
 import { TextParallaxContentExample } from "@/components/ui/Features";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -15,11 +18,12 @@ export default function Home() {
     
     <>
     
-
+    <ShuffleHero/>
     <TextParallaxContentExample/>
     
     <StickyScrollRevealDemo/>
     <ChakraProvider>
+    <GridBlurredBackdrop/>
     <Questions/>
     <div> 
     <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A1310ee24fa0e4717f56a401a049c366818ae5442e279ee96ee2a602ef6a80227&amp;source=constructor" width="100%" height="400" ></iframe>
